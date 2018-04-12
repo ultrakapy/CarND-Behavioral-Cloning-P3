148cdfa7f2f8543fee64339ea39fa1b9a19e42bb
ultrakapy <kapambwe@gmail.com>
2018-04-12 09:47:05 +0000

    Rename model code file

--- model.py
+++ model.py
@@ -112,8 +112,20 @@ model.add(Dense(1))
 #model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
-model.fit_generator(train_generator, samples_per_epoch= \
+history_object = model.fit_generator(train_generator, samples_per_epoch= \
                     len(train_samples), validation_data=validation_generator, \
                     nb_val_samples=len(validation_samples), nb_epoch=4)
 
 model.save('model.h5')
+
+### plot the training and validation loss for each epoch
+#from keras.models import Model
+#import matplotlib.pyplot as plt
+
+#plt.plot(history_object.history['loss'])
+#plt.plot(history_object.history['val_loss'])
+#plt.title('model mean squared error loss')
+#plt.ylabel('mean squared error loss')
+#plt.xlabel('epoch')
+#plt.legend(['training set', 'validation set'], loc='upper right')
+#plt.show()
